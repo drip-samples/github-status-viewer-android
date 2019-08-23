b95a0cf606620c0c41324b965473933385434e76
Ogawa Mitsunori <mitsunori.ogawa@drecom.co.jp>
2019-08-23 19:43:59 +0900

    M: androidx 移行して viewmodel livedata を入れた

--- app/src/main/java/jp/co/drip/sample/githubstatusviewer/MainActivity.kt
+++ app/src/main/java/jp/co/drip/sample/githubstatusviewer/MainActivity.kt
@@ -1,6 +1,6 @@
 package jp.co.drip.sample.githubstatusviewer
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
